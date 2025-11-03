6ce2a2f109de9271c354715c7d613d974332a5a0
thibolu-uofa <lutellie@ualberta.ca>
2025-11-03 10:52:04 -0700

    Add a utility file as example

--- app/src/main/java/com/example/myapplication/SLinkedUtils.java
+++ app/src/main/java/com/example/myapplication/SLinkedUtils.java
@@ -3,4 +3,8 @@ package com.example.myapplication;
 public class SLinkedUtils {
 
     // There will be some utilities here
+
+    public static void reverse(SLinkedList list) {
+        // going to do something
+    }
 }
